f192137d416420d0484e47e131021555ea4ade0c
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-10-04 10:52:21 +0530

    Setup the validation

--- ts/main.ts
+++ ts/main.ts
@@ -1,5 +1,28 @@
 import $ from 'jquery';
 
-$(()=>{
-    $('#txt-id').trigger('focus');
+$('#txt-id').trigger('focus');
+
+$('#btn-save').on('click', ()=> {
+
+    let id = $('#txt-id').val() as string;
+    let name = $('#txt-name').val() as string;
+    let address = $('#txt-address').val() as string;
+
+    $('#txt-id, #txt-name, #txt-address').parent().removeClass('invalid');
+
+    if (address.trim().length < 3){
+        $('#txt-address').parent().addClass('invalid');
+        $('#txt-address').trigger('select');
+    }
+
+    if (!/[A-Za-z .]+/.test(name.trim())){
+        $('#txt-name').parent().addClass('invalid');
+        $('#txt-name').trigger('select');
+    }
+
+    if (!/^C\d{3}$/.test(id.trim())){
+        $('#txt-id').parent().addClass('invalid');
+        $('#txt-id').trigger('select');
+    }
+
 });
